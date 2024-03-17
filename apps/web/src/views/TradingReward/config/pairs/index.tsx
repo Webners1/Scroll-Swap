09abc633076eee62439e15e7094c4a6da8b46124
ShayanIrfan <ishayan767@gmail.com>
2024-03-17 23:12:44 +0500

    add contract

--- apps/web/src/views/TradingReward/config/pairs/index.tsx
+++ apps/web/src/views/TradingReward/config/pairs/index.tsx
@@ -29,4 +29,5 @@ export const tradingRewardPairConfigChainMap: Record<FarmV3SupportedChainId, Com
   [ChainId.OPBNB_TESTNET]: opBNBTestnetFarms,
   [ChainId.OPBNB]: opBNBFarms,
   [ChainId.SCROLL_SEPOLIA]: scrollSepoliaFarm,
+  [ChainId.SCROLL]: [],
 }
